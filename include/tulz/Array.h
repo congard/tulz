a486e382e77f233797302aeb70efeab69cfdb29d
Daniel <dbcongard@gmail.com>
2022-05-21 13:53:46 +0200

    Improve container
    * remove useless const
    * malloc instead of new
    * add Array::resize
    * add Array::empty
    * add ability to set initial values
    * make Array(T*, size) more secure

--- include/tulz/Array.h
+++ include/tulz/Array.h
@@ -2,28 +2,43 @@
 #define TULZ_ARRAY_H
 
 #include <cstddef>
+#include <cstring>
 #include <algorithm>
 
 namespace tulz {
 
 /**
- * Container for C static array
+ * Container for C array
  * @tparam T
  */
 template <typename T>
 class Array {
 public:
+    using value_type = T;
+
     T *m_array = nullptr;
     size_t m_size = 0;
 
-    explicit Array(T *const array, const size_t size) {
-        m_array = array;
+    Array(T *array, size_t size, bool copy = true) {
+        if (copy) {
+            m_array = static_cast<T*>(malloc(size * sizeof(T)));
+            memcpy(m_array, array, size * sizeof(T));
+        } else { // Warning: dangerous since we don't know how array was created
+            m_array = array;
+        }
+
         m_size = size;
     }
 
-    explicit Array(const size_t size) {
+    explicit Array(size_t size) {
         m_size = size;
-        m_array = new T[size];
+        m_array = static_cast<T*>(malloc(size * sizeof(T)));
+    }
+
+    Array(size_t size, const T &value): Array(size) {
+        for (size_t i = 0; i < size; ++i) {
+            m_array[i] = value;
+        }
     }
 
     Array() = default;
@@ -31,9 +46,11 @@ public:
     // copy constructor
     Array(const Array<T> &src) {
         m_size = src.m_size;
-        m_array = new T[m_size];
-        for (size_t i = 0; i < m_size; i++)
+        m_array = static_cast<T*>(malloc(m_size * sizeof(T)));
+
+        for (size_t i = 0; i < m_size; i++) {
             m_array[i] = src.m_array[i];
+        }
     }
 
     // move constructor
@@ -55,23 +72,44 @@ public:
     }
 
     ~Array() {
-        delete[] m_array;
+        free(m_array);
     }
 
-    T& operator[](const size_t index) {
+    T& operator[](size_t index) {
         return m_array[index];
     }
 
-    const T& operator[](const size_t index) const {
+    const T& operator[](size_t index) const {
         return m_array[index];
     }
 
-    inline T* array() const {
+    void resize(size_t size) {
+        m_array = static_cast<T*>(realloc(m_array, size * sizeof(T)));
+        m_size = size;
+    }
+
+    void resize(size_t size, const T &value) {
+        m_array = static_cast<T*>(realloc(m_array, size * sizeof(T)));
+
+        if (size > m_size) {
+            for (size_t i = m_size; i < size; ++i) {
+                m_array[i] = value;
+            }
+        }
+
+        m_size = size;
+    }
+
+    T* array() const {
         return m_array;
     }
 
-    inline size_t size() const {
+    size_t size() const {
         return m_size;
+    }
+
+    bool empty() const {
+        return m_size == 0;
     }
 
     void swap(Array<T> &other) {
