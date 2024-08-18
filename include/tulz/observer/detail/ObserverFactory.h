593be846ad278e4443be470255c7a0efd6df6f21
Daniel <dbcongard@gmail.com>
2024-08-18 18:29:42 +0200

    Fix invalid constraints & improve code readability & add doc

--- include/tulz/observer/detail/ObserverFactory.h
+++ include/tulz/observer/detail/ObserverFactory.h
@@ -8,32 +8,44 @@
 
 namespace tulz::detail {
 template<typename F, typename ...Args>
-concept IsObserverFactoryFunctor = requires(F f, Args... args) {
-    ObserverPtr<Args...>{f(std::forward<Args>(args)...)};
+concept IsObserverFactoryFunctor = requires(F f, Observer<Args...>::Func callable) {
+    ObserverPtr<Args...>{f.template operator()<Args...>(std::move(callable))};
 };
 
 /**
- * @tparam F The functor that produces observer of desired type
+ * @tparam F The functor that produces observer of desired type.
+ * @tparam Args The arguments that will be accepted by the produced observer.
+ * @see `IsObserverFactoryFunctor`
  */
 template<typename F, typename ...Args>
 struct ObserverFactory {
+    using ObserverFunction = typename Observer<Args...>::Func;
+    using ObserverSelfView = typename Observer<Args...>::SelfView;
+
+    /**
+     * "Simple" or "Passthrough" operator().
+     */
     template<typename C>
     auto operator()(C &&callable)
         requires IsObserverFactoryFunctor<F, Args...> &&
-                 std::invocable<C, Args...>
+                 std::convertible_to<C, ObserverFunction>
     {
         F factory{};
         return factory.template operator()<Args...>(std::forward<C>(callable));
     }
 
+    /**
+     * Produces an observer with an additional `self` argument.
+     * @tparam C The callable that accepts `SelfView` as its first argument.
+     */
     template<typename C>
     auto operator()(C &&callable)
         requires IsObserverFactoryFunctor<F, Args...> &&
-                 std::invocable<C, typename Observer<Args...>::SelfView, Args...>
+                 std::invocable<C, ObserverSelfView, Args...>
     {
         F factory{};
 
-        ObserverPtr<Args...> observer = factory.template operator()<Args...>();
+        ObserverPtr<Args...> observer {factory.template operator()<Args...>()};
         *observer = [self = observer.get(), callable = std::forward<C>(callable)](Args... args) {
             callable(self, std::forward<Args>(args)...);
         };
