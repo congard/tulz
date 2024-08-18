ffded143f4d07436a0c5c17ababc5315f1b81d41
Daniel <dbcongard@gmail.com>
2024-08-18 18:32:27 +0200

    Update tests

--- tests/tulz/observer/EternalObserverTest.cpp
+++ tests/tulz/observer/EternalObserverTest.cpp
@@ -10,8 +10,8 @@ TEST(EternalObserverTest, SelfInvalidate) {
     Subject<> subject;
     int counter {0};
 
-    using Self = EternalObserver<>::SelfView;
-    auto observer = EternalObserverFactory<>{}([&counter](Self self) {
+    using Observer = EternalObserver<>;
+    auto observer = Observer::Factory{}([&counter](Observer::SelfView self) {
         ++counter;
         self->invalidate();
     });
@@ -27,4 +27,27 @@ TEST(EternalObserverTest, SelfInvalidate) {
     subject.notify();
 
     ASSERT_EQ(counter, 1);
+}
+
+TEST(EternalObserverTest, ProducedByFactory) {
+    Subject<int> subject;
+
+    int value {0};
+
+    using Observer = EternalObserver<int>;
+    auto observer = Observer::Factory{}([&value](int i) {
+        value = i;
+    });
+
+    auto subscription = subject.subscribe(std::move(observer));
+
+    subject.notify(1);
+
+    ASSERT_EQ(value, 1);
+
+    subscription.unsubscribe();
+
+    subject.notify(2);
+
+    ASSERT_EQ(value, 1);
 }
